12beb21aa717676156bae454a2f0cecad19710be
Pedro Guimarães <tigreped@gmail.com>
2013-09-02 09:25:44 -0300

    First commit.

---
+++ scripts/myplaylist.js
@@ -0,0 +1,187 @@
+/**
+ * Created by 23rd and Walnut
+ * www.23andwalnut.com
+ * User: Saleem El-Amin
+ * Date: 6/8/11
+ * Time: 9:39 AM
+ */
+
+var tracklist = [
+    {
+        mp3:'http://archive.org/download/HazamatNossaVoz/Hazamat%20-%20Nossa%20Voz.mp3',
+        title:'Nossa voz [Single]',
+        artist:'Hazamat'
+    },
+    {
+        mp3:'http://hazamat.com/mp3/Hazamat%20-%2001%20-%20Ultima%20noite.mp3',
+        title:'01 - Última Noite [Hazamat]',
+        artist:'Hazamat'
+    },
+    {
+        mp3:'http://hazamat.com/mp3/Hazamat%20-%2002%20-%20O%20lider.mp3',
+        title:'02 - O líder[Hazamat]',
+        artist:'Hazamat'
+    },
+    {
+        mp3:'http://hazamat.com/mp3/Hazamat%20-%2003%20-%20Danado.mp3',
+        title:'03 - Danado [Hazamat]Luminosa',
+        artist:'Hazamat'
+    },
+    {
+        mp3:'http://hazamat.com/mp3/Hazamat%20-%2004%20-%20Confessional.mp3',
+        title:'04 - Confessional [Hazamat]',
+        artist:'Hazamat'
+    },
+    {
+        mp3:'http://hazamat.com/mp3/Hazamat%20-%2005%20-%20Astrolabios.mp3',
+        title:'05 - Astrolábios [Hazamat]',
+        artist:'Hazamat'
+    },
+    {
+        mp3:'http://hazamat.com/mp3/Hazamat%20-%2006%20-%20Miriades.mp3',
+        title:'06 - Miríades [Hazamat]',
+        artist:'Hazamat'
+    },
+    {
+        mp3:'http://hazamat.com/mp3/Hazamat%20-%2007%20-%20Luminosa.mp3',
+        title:'07 - Luminosa [Hazamat]',
+        artist:'Hazamat'
+    },
+    {
+        mp3:'http://hazamat.com/mp3/Hazamat%20-%2008%20-%20Sobre%20a%20terra.mp3',
+        title:'08 - Sobre a terra [Hazamat]',
+        artist:'Hazamat'
+    },
+    {
+        mp3:'http://hazamat.com/mp3/Hazamat%20-%2009%20-%20Sob%20o%20sol.mp3',
+        title:'09 - Sob o sol [Hazamat]',
+        artist:'Hazamat'
+    },
+    {
+        mp3:'http://hazamat.com/mp3/Hazamat%20-%2010%20-%20Mazorca.mp3',
+        title:'10 - Mazorca [Hazamat]',
+        artist:'Hazamat'
+    },
+    {
+        mp3:'http://hazamat.com/mp3/Hazamat%20-%2011%20-%20Cativeiro.mp3',
+        title:'11 - Cativeiro [Hazamat]',
+        artist:'Hazamat'
+    },
+    {   
+        mp3:'http://hazamat.com/mp3/Hazamat%20-%2012%20-%20Demissao.mp3',
+        title:'12 - Demissão [Hazamat]',
+        artist:'Hazamat'
+    },
+    {
+        mp3:'http://hazamat.com/mp3/Hazamat%20-%2013%20-%20Memorias%20e%20correntes.mp3',
+        title:'13 - Memórias e correntes [Hazamat]',
+        artist:'Hazamat'
+    },
+    {
+        mp3:'http://archive.org/download/Hazamat-LiveAtTapero-pb/Hazamat-01-LamentoSertanejo.ogg',
+        title:'01 - Lamento sertanejo [Live at Taperoá-PB]',
+        artist:'Hazamat'
+    },
+    {
+        mp3:'http://archive.org/download/Hazamat-LiveAtTapero-pb/Hazamat-02-Danado.ogg',
+        title:'02 - Danado [Live at Taperoá-PB]',
+        artist:'Hazamat'
+    },
+    {
+        mp3:'http://archive.org/download/Hazamat-LiveAtTapero-pb/Hazamat-03-ltimaNoite.ogg',
+        title:'03 - Última noite [Live at Taperoá-PB]',
+        artist:'Hazamat'
+    },
+    {
+        mp3:'http://archive.org/download/Hazamat-LiveAtTapero-pb/Hazamat-04-Alerta.ogg',
+        title:'04 - Alerta [Live at Taperoá-PB]',
+        artist:'Hazamat'
+    },
+    {
+        mp3:'http://archive.org/download/Hazamat-LiveAtTapero-pb/Hazamat-05-Cativeiro.ogg',
+        title:'05 - Cativeiro [Live at Taperoá-PB]',
+        artist:'Hazamat'
+    },
+    {
+        mp3:'http://archive.org/download/Hazamat-LiveAtTapero-pb/Hazamat-06-Astrolbios.ogg',
+        title:'06 - Astrolábios [Live at Taperoá-PB]',
+        artist:'Hazamat'
+    },
+    {
+        mp3:'http://archive.org/download/Hazamat-LiveAtTapero-pb/Hazamat-07-Luminosa.ogg',
+        title:'07 - Luminosa [Live at Taperoá-PB]',
+        artist:'Hazamat'
+    },
+    {
+        mp3:'http://archive.org/download/Hazamat-LiveAtTapero-pb/Hazamat-08-LamentoMalungo.ogg',
+        title:'08 - Memórias e correntes [Live at Taperoá-PB]',
+        artist:'Hazamat'
+    },
+    {
+        mp3:'http://archive.org/download/Hazamat-LiveAtTapero-pb/Hazamat-09-GalosNoitesEQuintais.ogg',
+        title:'09 - Galos, noites e quintais [Live at Taperoá-PB]',
+        artist:'Hazamat'
+    },
+    {
+        mp3:'http://archive.org/download/Hazamat-LiveAtTapero-pb/Hazamat-10-BomSerAve.ogg',
+        title:'10 - É bom ser ave [Live at Taperoá-PB]',
+        artist:'Hazamat'
+    },
+    {
+        mp3:'http://archive.org/download/Hazamat-LiveAtTapero-pb/Hazamat-11-VejaMargarida.ogg',
+        title:'11 - Veja Margarida [Live at Taperoá-PB]',
+        artist:'Hazamat'
+    },
+    {   
+        mp3:'http://archive.org/download/Hazamat-LiveAtTapero-pb/Hazamat-12-VozQueVem.ogg',
+        title:'12 - Voz que vem [Live at Taperoá-PB]',
+        artist:'Hazamat'
+    },
+    {
+        mp3:'http://archive.org/download/Hazamat-LiveAtTapero-pb/Hazamat-13-GalopeRasante.ogg',
+        title:'13 - Galope rasante [Live at Taperoá-PB]',
+        artist:'Hazamat'
+    },
+    {
+        mp3:'http://archive.org/download/Hazamat-LiveAtTapero-pb/Hazamat-14-OLder.ogg',
+        title:'14 - O líder [Live at Taperoá-PB]',
+        artist:'Hazamat'
+    },
+    {   
+        mp3:'http://archive.org/download/Hazamat-LiveAtTapero-pb/Hazamat-15-NossaVoz.ogg',
+        title:'15 - Nossa voz [Live at Taperoá-PB]',
+        artist:'Hazamat'
+    },
+    {
+        mp3:'http://archive.org/download/Hazamat-LiveAtTapero-pb/Hazamat-16-SobOSol.ogg',
+        title:'16 - Sob o sol [Live at Taperoá-PB]',
+        artist:'Hazamat'
+    }
+];
+
+/* Takes the track list and returns a randomized list of the tracks */
+function randomList() {
+    var amount = tracklist.length;
+    var notpicked = new Array();
+    var randomList = new Array();
+    for (var i=0 ; i < amount ; i++)
+        notpicked.push(i);
+    while (notpicked.length > 0) {
+        var arandom = notpicked[Math.floor((Math.random()*(notpicked.length-1))+0)];
+        randomList.push(tracklist[arandom]);
+        notpicked = removeItem(arandom, notpicked);
+    }
+    return randomList;
+}
+
+/* Remove the item from the list */
+function removeItem(item, list) {
+    var newArray = new Array();
+    for (var i=0; i<list.length;i++) {
+        if (list[i]!=item)
+            newArray.push(list[i]);
+    }
+    return newArray;
+}
+
+var myPlaylist = randomList(); 
